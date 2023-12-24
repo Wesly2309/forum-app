3bd77b663f6cfed1c3b3751e87333a3670befecb
Wesly2309 <weslylims02@gmail.com>
2023-12-24 19:55:32 +0700

    final commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\AuthenticationController;
 use App\Http\Controllers\Feed\FeedController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/feeds', [FeedController::class, 'index'])->middleware('auth:sanctum');
 Route::post('/feed/store', [FeedController::class, 'store'])->middleware('auth:sanctum');
 Route::post('/feed/like/{feed_id}', [FeedController::class, 'likePost'])->middleware('auth:sanctum');
-Route::post('/feed/comments/{feed_id}', [FeedController::class, 'comment'])->middleware('auth:sanctum');
+Route::post('/feed/comment/{feed_id}', [FeedController::class, 'comment'])->middleware('auth:sanctum');
 Route::get('/feed/comments/{feed_id}', [FeedController::class, 'getComments'])->middleware('auth:sanctum');
 
 Route::get('/test', function () {
